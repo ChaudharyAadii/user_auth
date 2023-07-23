2a67d26bcf89583c5023a44b41d50164b0a191d4
Chaudhary Aditya Singh <chaudharyaditya1232@gmail.com>
2023-07-23 16:23:38 +0530

    Update Dashboard.js
    Adding more details to the user dashboard page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,25 +1,61 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 export default function Dashboard() {
 
+  const [name, setName] = useState("");
+
   const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const email = location.state;
+
+  // const getUsers = async () => {
+  //   const response = await fetch('server.chaudharyaadii.repl.co', {
+  //     method: 'GET',
+  //   })
+  //   const data = await response.json()
+  //   // console.log(form);
+  //   console.log(data);
+  //   setName(data);
+  // }
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000?email=${email}`, {
+        method: 'GET',
+      });
+      const data = await response.json();
+      console.log(data);
+      setName(data.fname);
+      console.log(name);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [email])
 
   const userValid = () => {
     let token = localStorage.getItem("userdbtoken");
-    if(token){
+    if (token) {
       console.log("User valid");
-    } else{
+    } else {
       navigate("*")
     }
   }
 
   useEffect(() => {
     userValid();
-  },[])
+  }, [])
   return (
     <>
-    <h1 style={{"text-align": "center"}} className='mt-5'>Congrats !! You have successfully logged in</h1>
+    <h1 style={{ "text-align": "center", "margin-top": "20px"}}>Hello {name}</h1> <br />
+      <h2 style={{ "text-align": "center" }} className='mt-5'>Congrats !! You have successfully logged in as <br />
+        {email} with username as {name}</h2>
     </>
   )
 }
